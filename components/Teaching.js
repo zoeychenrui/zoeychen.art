79a0c087c0fa55479b8ef007aa0f28ff7b78e2da
zoeychenrui <zoeychen2013@gmail.com>
2023-07-24 14:57:47 -0400

    start teaching

--- components/Teaching.js
+++ components/Teaching.js
@@ -1,59 +1,66 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import storage from "./Config";
 
-function gallery(props) {
-
-    const [allImages, setImages] = useState([]);
-
-    const getFromFirebase = (id) => {
-    let storageRef = storage.ref().child(id);
-    storageRef.listAll().then(function (res) {
-        res.items.forEach((imageRef) => {
-          imageRef.getDownloadURL().then((url) => {
-              setImages((allImages) => [...allImages, url]);
-          });
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-  console.log(allImages);
-
-useEffect(() => {
-    getFromFirebase();
-   
-}, []);
-
-    return (
-      <figure class="group cursor-pointer mt-4">
-        <div class="relative overflow-hidden">
-          <img
-            src={props.src}
-            class="ease absolute z-0 h-full w-full object-cover
-            opacity-0 transition-opacity duration-700 group-hover:opacity-100"
-            alt="Piece"
-            
-          />
-    
-          <div class="h-full w-full object-cover italic" >
-            Click to see class gallery
-          </div>
-
-        </div>
-  
-        <figcaption className="mt-1">
-          <span class=" text-gray-500">{props.text}</span>
-        </figcaption>
-      </figure>
-    );
-  }
 
 function Teaching() {
+  const [allImages, setImages] = useState([]);
+  const [selectedGallery, setSelectedGallery] = useState(null);
+  
+  const handleGalleryClick = (gallery) => {
+    setSelectedGallery(gallery);
+  };
 
+  const getFromFirebase = (id) => {
+  let storageRef = storage.ref().child(id);
+  storageRef.listAll().then(function (res) {
+      res.items.forEach((imageRef) => {
+        imageRef.getDownloadURL().then((url) => {
+            setImages((allImages) => [...allImages, url]);
+        });
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+console.log(allImages);
+
+useEffect(() => {
+  getFromFirebase();
+ 
+}, []);
   return (
     <div>
-      
+
+    <div className={`flex w-[95%] md:w-[85%] lg:w-[70%] mx-auto mt-12 lg:mt-24 mb-48 `}>
+  <div className="w-[35%] p-4 mr-2 md:mr-6 lg:mr-24">
+    <h2 className="text-xl font-bold mb-4 px-2 mt-2 text-white">Galleries</h2>
+    <ul>
+      {galleries.map((gallery, index) => (
+        <li
+          key={index}
+          className={`py-2 px-2 cursor-pointer ${
+            selectedGallery === gallery ? 'font-bold ' : 'font-light'
+          }`}
+          onClick={() => handleGalleryClick(activity)}
+        >
+          {gallery.name}
+        </li>
+      ))}
+    </ul>
+  </div>
+  <div className="w-[65%] p-4">
+    {selectedGallery ? (
+      <>
+        <h2 className={`text-xl font-bold mb-4`}>{selectedGallery.name}</h2>
+        
+      </>
+    ) : (
+      <p className="text-xl">Select a class to see the student gallery</p>
+    )}
+  </div>
+</div>
+    <Footer />
     </div>
   )
 }
