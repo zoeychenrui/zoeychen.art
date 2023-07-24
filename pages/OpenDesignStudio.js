b8ac082eedf1ad5dca386a40b3f68c6f73a705d7
zoeychenrui <zoeychen2013@gmail.com>
2023-07-24 18:09:14 -0400

    open design start

--- pages/OpenDesignStudio.js
+++ pages/OpenDesignStudio.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function OpenDesignStudio() {
   return (
-    <div>
-      
+    <div className="w-[80%] mx-auto ">
+      <div className="text-3xl md:text-4xl lg:text-5xl text-gray-400 mt-12 ml-2">Logo Explorations</div>
+      <img></img>
     </div>
   )
 }
